0d752d23bbf243135547930f3cf597b017a4209c
renangbez <renangbez@gmail.com>
2024-05-02 21:58:57 -0300

    espaco

--- exercicio16.js
+++ exercicio16.js
@@ -4,7 +4,7 @@ let numero = 100;
 let contador = 0;
 
 while (contador < 50) {
-    if ((numero % 2 != 0) && (numero % 3 != 0)){
+    if ((numero % 2 != 0) && (numero % 3 != 0)) {
         console.log(numero);
         contador += 1;
     } 
